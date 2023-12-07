3d18592f2110c6fcf1f25771231b2e885c934367
LillKonst <konstlillkristine@gmail.com>
2023-12-07 14:01:54 +0100

    lots of update

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,20 @@
 document.querySelector(".hamburger-menu").addEventListener("click", function() {
     document.querySelector(".menu").classList.toggle("active");
   });
+
+  
+  const portfolio = document.querySelector('.project__container');
+portfolio.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+const aboutMe = document.querySelector('.about-me');
+aboutMe.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
