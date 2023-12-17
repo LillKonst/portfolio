daa7e5fc50e56ea72fdbb320d104bd39df0249d1
LillKonst <konstlillkristine@gmail.com>
2023-12-17 16:19:17 +0100

    header, menu, colors

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,49 @@
-document.querySelector(".hamburger-menu").addEventListener("click", function() {
-    document.querySelector(".menu").classList.toggle("active");
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+    positionMenu(); // Trigger position update on menu toggle
   });
+  
+  window.addEventListener('scroll', () => {
+    const header = document.querySelector(".hamburger-menu");
+    
+    if (window.scrollY > 0) {
+      header.classList.add("sticky");
+      positionMenu(); // Update menu position on scroll
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+  
+  // Code for header on desktop 
+
+  function toggleHeaderOnScroll() {
+    const header = document.querySelector("header");
+  
+    if (window.innerWidth > 1000) { // Adjust this width according to your desktop breakpoint
+      if (window.scrollY > 0) {
+        header.classList.add("fixed-header");
+      } else {
+        header.classList.remove("fixed-header");
+      }
+    }
+  }
+  
+  window.addEventListener('scroll', () => {
+    toggleHeaderOnScroll();
+  });
+  
+  window.addEventListener('resize', () => {
+    toggleHeaderOnScroll();
+  });
+  
+  // Initialize the behavior on page load
+  toggleHeaderOnScroll();
+  
+
+
+
+
 
   
   const portfolio = document.querySelector('.project__container');
